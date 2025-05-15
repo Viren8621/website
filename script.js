324eeae40d7ece40020bcd085aae71cdb2b55b21
Viren8621 <virenmore8621@gmail.com>
2025-05-15 19:27:35 +0530

    contact added

--- script.js
+++ script.js
@@ -6,11 +6,5 @@ const hero = document.querySelector('.hero');
 const images = ['images/slide1.jpg'];
 let current = 0;
 
-// JavaScript for mobile menu toggle
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.querySelector('.nav-links');
 
-menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
 
